886aed7c07c3cf9ffc3ee9c405739843d71854b2
sim <jasm0012@interia.pl>
2022-05-14 15:44:19 +0200

    added core structure

---
+++ src/main/java/com/example/tictactoe/TicActions.java
@@ -0,0 +1,20 @@
+package com.example.tictactoe;
+
+public class TicActions {
+    public String a;
+    private String b;
+
+    public String A1() { return a = "a"; }
+
+    public String A2() { return a = "b"; }
+
+    public String A3() { return a = "c"; }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+}
--- src/main/java/com/example/tictactoe/TicController.java
+++ src/main/java/com/example/tictactoe/TicController.java
@@ -1,13 +1,39 @@
 package com.example.tictactoe;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TicController {
     @GetMapping("/")
-    public String showForm() {
+    public String showForm(Model model) {
+        TicActions ticActions = new TicActions();
+        model.addAttribute("ticActions", ticActions);
         return "start";
     }
 
+    @PostMapping("/")
+    public String submitForm6(@ModelAttribute TicActions ticActions, @RequestParam(required = false) String name) {
+        System.out.println(ticActions);
+        if (name.equals("A1")){
+            ticActions.a = "a";
+            return "start";
+        }
+        if (name.equals("A2")){
+            ticActions.a = "b";
+            return "start";
+        }
+        if (name.equals("A3")) {
+            ticActions.a = "c";
+            return "start";
+        }
+        else {
+            ticActions.a = "0";
+            return "start";
+        }
+    }
 }
