4b307069d62987b57f43b15b02837d7efa668b85
sim <jasm0012@interia.pl>
2022-05-15 14:34:09 +0200

    added table

--- src/main/java/com/example/tictactoe/TicActions.java
+++ src/main/java/com/example/tictactoe/TicActions.java
@@ -1,20 +1,104 @@
 package com.example.tictactoe;
 
 public class TicActions {
-    public String a;
-    private String b;
+    public static String xa = " ";
+    public static String xb = " ";
+    public static String xc = " ";
+    public static String xd = " ";
+    public static String xe = " ";
+    public static String xf = " ";
+    public static String xg = " ";
+    public static String xh = " ";
+    public static String xi = " ";
 
-    public String A1() { return a = "a"; }
+    public String A1() { return xa; }
 
-    public String A2() { return a = "b"; }
+    public String A2() { return xb; }
 
-    public String A3() { return a = "c"; }
+    public String A3() { return xc; }
 
-    public String getA() {
-        return a;
+    public String B1() { return xd; }
+
+    public String B2() { return xe; }
+
+    public String B3() { return xf; }
+
+    public String C1() { return xg; }
+
+    public String C2() { return xh; }
+
+    public String C3() { return xi; }
+
+    public static String getXa() {
+        return xa;
     }
 
-    public void setA(String a) {
-        this.a = a;
+    public static void setXa(String xa) {
+        TicActions.xa = xa;
     }
+
+    public static String getXb() {
+        return xb;
+    }
+
+    public static void setXb(String xb) {
+        TicActions.xb = xb;
+    }
+
+    public static String getXc() {
+        return xc;
+    }
+
+    public static void setXc(String xc) {
+        TicActions.xc = xc;
+    }
+
+    public String getXd() {
+        return xd;
+    }
+
+    public void setXd(String xd) {
+        this.xd = xd;
+    }
+
+    public String getXe() {
+        return xe;
+    }
+
+    public void setXe(String xe) {
+        this.xe = xe;
+    }
+
+    public String getXf() {
+        return xf;
+    }
+
+    public void setXf(String xf) {
+        this.xf = xf;
+    }
+
+    public String getXg() {
+        return xg;
+    }
+
+    public void setXg(String xg) {
+        this.xg = xg;
+    }
+
+    public String getXh() {
+        return xh;
+    }
+
+    public void setXh(String xh) {
+        this.xh = xh;
+    }
+
+    public String getXi() {
+        return xi;
+    }
+
+    public void setXi(String xi) {
+        this.xi = xi;
+    }
+
 }
--- src/main/java/com/example/tictactoe/TicController.java
+++ src/main/java/com/example/tictactoe/TicController.java
@@ -19,21 +19,103 @@ public class TicController {
     @PostMapping("/")
     public String submitForm6(@ModelAttribute TicActions ticActions, @RequestParam(required = false) String name) {
         System.out.println(ticActions);
+        System.out.println(name);
+
         if (name.equals("A1")){
-            ticActions.a = "a";
+            ticActions.xa = "X";
+            if (ticActions.xc.equals(" ")){
+                ticActions.xc = "O";
+            }
+            else {
+                if (ticActions.xi.equals(" ")) {
+                    ticActions.xi = "O";
+                }
+                else {
+                    if (ticActions.xg.equals(" ")) {
+                        ticActions.xg = "O";
+                    }
+                    else {
+                        if (ticActions.xg.equals(" ")) {
+                            ticActions.xg = "O";
+                        }
+                        else {
+                            if (ticActions.xb.equals(" ")) {
+                                ticActions.xb = "O";
+                            }
+                            else {
+                                if (ticActions.xd.equals(" ")) {
+                                    ticActions.xd = "O";
+                                }
+                                else {
+                                    if (ticActions.xe.equals(" ")) {
+                                        ticActions.xe = "O";
+                                    }
+                                    else {
+                                        if (ticActions.xf.equals(" ")) {
+                                            ticActions.xf = "O";
+                                        }
+                                        else {
+                                            if (ticActions.xh.equals(" ")) {
+                                                ticActions.xh = "O";
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
             return "start";
         }
         if (name.equals("A2")){
-            ticActions.a = "b";
+            ticActions.xb = "X";
             return "start";
         }
         if (name.equals("A3")) {
-            ticActions.a = "c";
+            ticActions.xc = "X";
             return "start";
         }
-        else {
-            ticActions.a = "0";
+        if (name.equals("B1")){
+            ticActions.xd = "X";
             return "start";
         }
+        if (name.equals("B2")){
+            ticActions.xe = "X";
+            return "start";
+        }
+        if (name.equals("B3")) {
+            ticActions.xf = "X";
+            return "start";
+        }
+        if (name.equals("C1")){
+            ticActions.xg = "X";
+            return "start";
+        }
+        if (name.equals("C2")){
+            ticActions.xh = "X";
+            return "start";
+        }
+        if (name.equals("C3")) {
+            ticActions.xi = "X";
+            return "start";
+        }
+        else  {
+            ticActions.xa = " ";
+            ticActions.xb = " ";
+            ticActions.xc = " ";
+            ticActions.xd = " ";
+            ticActions.xe = " ";
+            ticActions.xf = " ";
+            ticActions.xg = " ";
+            ticActions.xh = " ";
+            ticActions.xi = " ";
+            return "start";
+        }
+//        else {
+//            ticActions.xi = "y";
+//            return "start";
+//        }
+//        return name;
     }
 }
