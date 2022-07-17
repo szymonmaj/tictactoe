efc911aa20a85a8701829e53aad0f6df5bfa4a43
sim <jasm0012@interia.pl>
2022-07-18 00:20:59 +0200

    added console board

--- src/main/java/com/example/tictactoe/TictactoeApplication.java
+++ src/main/java/com/example/tictactoe/TictactoeApplication.java
@@ -10,4 +10,29 @@ public class TictactoeApplication {
 		SpringApplication.run(TictactoeApplication.class, args);
 	}
 
+	{
+		int dim = 3;
+		char[][] board = new char[dim][dim];
+		// wstawiam X i O w wybrane miejsca dla demonstracji
+		board[1][1] = 'X';
+		board[2][2] = 'O';
+		printBoard(board);
+	}
+
+	private static void printBoard(char[][] board) {
+		int dim = board.length;
+		// nagłówki kolumn
+		System.out.print("\t");
+		for (int i = 0; i < dim; i++) {
+			System.out.print(i + "\t");
+		}
+		System.out.println();
+		for (int row = 0; row < dim; row++) {
+			System.out.print(row + ":\t");
+			for (int column = 0; column < dim; column++) {
+				System.out.print(board[row][column] + "\t");
+			}
+			System.out.println();
+		}
+	}
 }
